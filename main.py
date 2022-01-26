bd70ab037c0a03a8b04dc33e1fd1c87c67763268
Mareks Misukovs <98460992+ShtiroonsMareks@users.noreply.github.com>
2022-01-26 12:22:16 +0000

    Izpildīts papilduzdevums

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""
 mala1=int(input("Ievadi kvadrāta malu (cm):"))
 if mala1>=5:
   kv_1=mala1*mala1
@@ -9,3 +10,16 @@ if mala1>=5:
 
 else:
   print("Ievadi lielāku skaitli")
+"""
+import math
+rinka_r= float(input("Ievadi riņķa rādiusa garumu (cm): "))
+tr_hip=float(input("Ievadi trijstūra hipotenūzas garumu (cm): "))
+
+mala_a=tr_hip*math.sin(math.radians(45))
+mala_b=math.sqrt(math.pow(tr_hip,2)-math.pow(mala_a,2))
+lauk_tr=(mala_a*mala_b)/2
+
+lauk_r=math.pi*math.pow(rinka_r,2)
+
+starpība=round(lauk_r-lauk_tr,1)
+print(f"Riņka laukums ir par {starpība} cm^2 lielāks nekā trijstūra laukums!")
